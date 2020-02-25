594c69e14dd7e7825b5346bb9f023b6ea36c9d38
Andrés Sanz Buezo <sanzbuezo@hotmail.com>
2020-02-25 01:13:40 +0100

    Crash Exercise 2
    Render a cube and refresh position.

---
+++ 02-Crash Exercise 2/scripts/script.js
@@ -0,0 +1,39 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Create the shape
+let geometry = new THREE.BoxGeometry(1,1,1);
+
+// Create a material, colour or image texture
+let material = new THREE.MeshBasicMaterial({
+        color: 0xFFFFFF,
+        wireframe: true
+});
+let cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 3;
+
+// Game Logic
+let update = function () {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.001;
+};
+
+// Draw scene
+let render = function () {
+    renderer.render(scene, camera);
+};
+
+// Run game loop
+let gameLoop = function () {
+    requestAnimationFrame(gameLoop);
+    update();
+    render();
+};
+
+gameLoop();
