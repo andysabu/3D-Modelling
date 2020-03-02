387ab1340bea5c1ae9f6841bb10031ce4fddaa5e
Andrés Sanz Buezo <sanzbuezo@hotmail.com>
2020-03-02 01:57:29 +0100

    Crash Exercise 4
    Add controls

---
+++ 04-Crash Exercise 4/scripts/script.js
@@ -0,0 +1,51 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener('resize', function () {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+});
+
+// Once the type of control is imported (OrbitControls.js),
+// it is implemented/added to the main render.
+controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+// Create the shape
+let geometry = new THREE.BoxGeometry(1,1,1);
+
+// Create a material, colour or image texture
+let material = new THREE.MeshBasicMaterial({
+        color: 0xFFFFFF,
+        wireframe: true
+});
+let cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 3;
+
+// Game Logic
+let update = function () {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.001;
+};
+
+// Draw scene
+let render = function () {
+    renderer.render(scene, camera);
+};
+
+// Run game loop
+let gameLoop = function () {
+    requestAnimationFrame(gameLoop);
+    update();
+    render();
+};
+
+gameLoop();
